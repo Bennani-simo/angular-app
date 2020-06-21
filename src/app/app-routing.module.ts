b8870a81a5a984ed9f7930ad2649b14f13c16c2c
Bennani Mohamed <simobenn96@gmail.com>
2020-06-21 20:37:48 +0200

    Routage ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactFormComponent } from './contact-list/contact-form/contact-form.component';
+import { SingleContactComponent } from './contact-list/single-contact/single-contact.component';
 
 
 
-const routes: Routes = [
-  //{ path: 'add', component: AddContactComponent },
-  //{ path: 'edit', component: EditContactComponent },
-  //{ path: 'home', component: HomeContactComponent }
+const Routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'contacts', component: ContactListComponent },
+  { path: 'contacts/new', component: ContactFormComponent },
+  { path: 'contacts/view/:id', component: SingleContactComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(Routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { SingleContactComponent } from './contact-list/single-contact/single-contact.component';
+import { ContactFormComponent } from './contact-list/contact-form/contact-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService } from './services/auth.service';
+import { ContactService } from './services/contact.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     SigninComponent,
+    ContactListComponent,
+    SingleContactComponent,
+    ContactFormComponent,
+    NavbarComponent,
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    ContactService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
