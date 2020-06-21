29efeef059d0ac507a6a14f4898c475181ee4e95
Bennani Mohamed <simobenn96@gmail.com>
2020-06-21 21:16:55 +0200

    Connect to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBW6FUVnwM3A_O6qcfyeL4s5wBuipE7xDo",
+      authDomain: "angular-app-34abe.firebaseapp.com",
+      databaseURL: "https://angular-app-34abe.firebaseio.com",
+      projectId: "angular-app-34abe",
+      storageBucket: "angular-app-34abe.appspot.com",
+      messagingSenderId: "637145471411",
+      appId: "1:637145471411:web:2e31b56c6fba7f396093c6",
+      measurementId: "G-4VVQ4L82TT"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
